5ccf461cd44b347c5c2732db246f76b93d3d9a91
amadzia <agnieszkamadzia@onet.pl>
2018-07-16 18:22:49 +0200

    first commit

---
+++ src/main/java/com/example/netfluxexamplewithgradle/NetfluxExampleWithGradleApplication.java
@@ -0,0 +1,12 @@
+package com.example.netfluxexamplewithgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxExampleWithGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetfluxExampleWithGradleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/netfluxexamplewithgradle/NetfluxExampleWithGradleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.netfluxexamplewithgradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetfluxExampleWithGradleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
