68fcde7dabb158340f66a1f73225df67372a1628
amadzia <agnieszkamadzia@onet.pl>
2018-07-17 12:55:32 +0200

    Data initialization. @AllArgsConstructor and dependency for webflux added

---
+++ src/main/java/com/example/netfluxexamplewithgradle/bootstrap/BootstrapCLR.java
@@ -0,0 +1,33 @@
+package com.example.netfluxexamplewithgradle.bootstrap;
+
+import com.example.netfluxexamplewithgradle.domain.Movie;
+import com.example.netfluxexamplewithgradle.repositories.MovieRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+
+@Component
+public class BootstrapCLR implements CommandLineRunner {
+
+    private final MovieRepository movieRepository;
+
+    public BootstrapCLR(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        movieRepository.deleteAll()
+                .thenMany(
+                        Flux.just("Silence", "Flux", "Mono",
+                                "Back to the future", "Lord of Dawn")
+                                .map(title -> new Movie(UUID.randomUUID().toString(), title))
+                                .flatMap(movieRepository::save))
+                .subscribe(null, null, () -> {
+                    movieRepository.findAll().subscribe(System.out::println);
+                });
+    }
+}
